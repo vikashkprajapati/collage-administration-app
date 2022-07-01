2c6e46652ae47981037927f6db3fc3fea09d0b07
vikashtechnoboost <Mr.vikash.kumar.vs@gmail.com>
2022-07-01 11:34:27 +0530

    basic project setup done

---
+++ src/main/java/com/collage/CollageApplication.java
@@ -0,0 +1,13 @@
+package com.collage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collage/CollageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
